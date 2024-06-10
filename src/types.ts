3e4d8d7fad76c9a5906c38800a17ba0422deb836
Richard Solomou <richard@solomou.io>
2024-06-10 21:01:11 +0300

    allow any for response

--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,10 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Schema } from '@typeschema/main';
 
 export type HandlerFunction<TParams, TQuery, TBody> = (
   request: Request,
   context: { params: TParams; query: TQuery; body: TBody },
-) => unknown;
+) => any;
 
 export interface RouteHandlerBuilderConfig {
   paramsSchema: Schema;
@@ -11,4 +12,4 @@ export interface RouteHandlerBuilderConfig {
   bodySchema: Schema;
 }
 
-export type OriginalRouteHandler = (request: Request, context?: { params: Record<string, unknown> }) => unknown;
+export type OriginalRouteHandler = (request: Request, context?: { params: Record<string, unknown> }) => any;
